ac71b9d7abe32da5c4ce6437ebff8b93338597c5
ruudvanderweijde <ruudvanderweijde@gmail.com>
2013-01-18 14:03:03 +0100

    Update Person.php

--- Person.php
+++ Person.php
@@ -26,13 +26,24 @@ class Person {
         $hit = rand(0,$weapon['damage']);
         $this->_health -= $hit;
 
-        $msg = $this->_name . " receives a hit with a {$weapon['weapon']} and looses {$hit}HP having {$this->_health}HP left<br>";
+        $msg = $this->_name . " receives a hit with a {$weapon['weapon']} and looses {$hit}HP having {$this->_health}HP left\r\n";
+       
         flush();
         @ob_flush();
         if ($this->_health<=0) {
             flush();
             $this->_setAlive(false);
-        }
+        } else {
+    	if (!rand(0,20)) {
+			if (rand(0,1)) {
+				$this->_health *= 2;
+				$msg = $this->_name . " finds a senzu bean, doubling his/her health to {$this->_health}HP.\r\n";
+			} else {
+				$this->_health = 100;
+				$msg = $this->_name . " finds a senzu bean, and has now {$this->_health}HP again.\r\n";
+			}
+		}
+	}
         return $msg;
     }
 
@@ -43,21 +54,33 @@ class Person {
     private function _getWeapon()
     {
         static $weapons = array(
+		array(
+                'weapon' => 'bitchslap in the face',
+                'damage' => '5',
+            ),
+		array(
+                'weapon' => 'Gaetan everywhere',
+                'damage' => '5',
+            ),
             array(
-                'weapon' => 'knife',
+                'weapon' => 'knife in the back',
                 'damage' => '10',
             ),
             array(
-                'weapon' => 'axe',
+                'weapon' => 'bullet in the knee',
+                'damage' => '10',
+            ),
+            array(
+                'weapon' => 'axe in the arm',
                 'damage' => '50',
             ),
             array(
-                'weapon' => 'sniper',
+                'weapon' => 'sniper in the head',
                 'damage' => '100',
             ),
             array(
                 'weapon' => 'kamehameha',
-                'damage' => '500',
+                'damage' => '150',
             ),
         );
         $size = count($weapons)-1;
